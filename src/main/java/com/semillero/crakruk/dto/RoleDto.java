0de58648fe00656d694dc073f4cafa22f700805d
Julian Greco <julian_greco@hotmail.com>
2022-11-08 20:06:04 -0300

    hotfix

--- src/main/java/com/semillero/crakruk/dto/RoleDto.java
+++ src/main/java/com/semillero/crakruk/dto/RoleDto.java
@@ -1,11 +1,19 @@
 package com.semillero.crakruk.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleDto {
     private Long id;
     private String name;
+
+    public RoleDto(String name){
+        this.name = name;
+    }
 }
